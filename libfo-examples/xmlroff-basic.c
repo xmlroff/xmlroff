1d1a9e49c19f78fc7f1a288c198a11098428d838
tkg <tkg@8c214379-a42c-0410-92c0-bc03c673cdb0>
2007-07-18 12:35:25 +0000

    Updated for libfo 0.5.2. (see #30)

--- libfo-examples/xmlroff-basic.c
+++ libfo-examples/xmlroff-basic.c
@@ -5,91 +5,106 @@
  * XML file into a PDF file without operating in terms of GObjects.
  *
  * Copyright (C) 2001-2004 Sun Microsystems
- *
- * $Id: xmlroff-basic.c,v 1.3 2004/10/20 23:34:07 tonygraham Exp $
+ * Copyright (C) 2007 Menteith Consulting Ltd
  *
  * See COPYING for the status of this software.
  */
 
 #include "config.h"
 #include <stdlib.h>
-#include <popt.h>
 #include <glib.h>
 #include <libfo/fo-libfo-basic.h>
 
 int
-main (int          argc,
-      const char **argv)
+main (gint    argc,
+      gchar **argv)
 {
-  poptContext optCon;   /* context for parsing command-line options */
+  GOptionContext *ctx;   /* context for parsing command-line options */
   FoLibfoContext *libfo_context;
   GError *error = NULL;
   gchar *out_file = "layout.pdf";
-  const gchar *xml_file, *xslt_file = NULL;
-  gint version = 0;
+  const gchar *xml_file = NULL;
+  const gchar *xslt_file = NULL;
+  gboolean version = FALSE;
+  gchar** files = NULL;
+  gboolean goption_success = FALSE;
 
-  const struct poptOption optionsTable[] = {
+  const GOptionEntry options[] = {
     { "out-file",
       'o',
-      POPT_ARG_STRING,
-      &out_file,
       0,
+      G_OPTION_ARG_STRING,
+      &out_file,
       "Output file",
       "filename"
     },
     { "version",
       'v',
-      POPT_ARG_NONE,
-      &version,
       0,
+      G_OPTION_ARG_NONE,
+      &version,
       "Print version number",
       NULL
     },
-    POPT_AUTOHELP
-    { NULL, '\0', 0, NULL, 0, NULL, NULL}
+    { G_OPTION_REMAINING,
+      0,
+      0,
+      G_OPTION_ARG_FILENAME_ARRAY,
+      &files,
+      NULL,
+      "file [stylesheet]"
+    },
+    {NULL, 0, 0, 0, NULL, NULL, NULL}
   };
 
-  optCon = poptGetContext (NULL, argc, argv, optionsTable, 0);
-  poptSetOtherOptionHelp (optCon, "xml-file [stylesheet]");
+  ctx = g_option_context_new (NULL);
+  g_option_context_add_main_entries (ctx, options, PACKAGE);
+  goption_success = g_option_context_parse (ctx, &argc, &argv, &error);
+  /* Finished with parsing command-line arguments. */
+  g_option_context_free(ctx);
+  ctx = NULL;
 
-  poptGetNextOpt (optCon);
+  if (goption_success == FALSE)
+    {
+      exit (1);
+    }
 
   if (version != 0)
     {
       g_print ("%s\nSubmit bug reports to %s\n",
 	       PACKAGE_STRING,
 	       PACKAGE_BUGREPORT);
+      if (files == NULL)
+	{
+	  /* Nothing to do if just asking for version. */
+	  exit (0);
+	}
     }
 
-  if (poptPeekArg (optCon))
+  if ((files == NULL) ||
+      (files[0] == NULL))
     {
-      xml_file = poptGetArg (optCon);
+      g_print ("No input file specified.\n");
+		  
+      exit (1);
     }
   else
     {
-      if (version != 0)
+      xml_file = files[0];
+    }
+
+  if (files[1] != NULL)
+    {
+      xslt_file = files[1];
+
+      if (files[2] != NULL)
 	{
-	  exit (0);
-	}
-      else
-	{
-	  poptPrintUsage(optCon, stderr, 0);
+	  g_print("Unexpected additional parameter: '%s'\n",
+		      files[2]);
+		  
 	  exit (1);
 	}
     }
-
-  if (poptPeekArg (optCon))
-    {
-      xslt_file = poptGetArg (optCon);
-    }
-
-  if (poptPeekArg (optCon))
-    {
-      poptPrintUsage(optCon, stderr, 0);
-      exit (1);
-    }
-
-  poptFreeContext (optCon);
 
   fo_libfo_init ();
 
