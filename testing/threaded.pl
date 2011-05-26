674ce8aa0a09c0c1c860f3ee6acbb964a34207f8
tkg <tkg@8c214379-a42c-0410-92c0-bc03c673cdb0>
2011-05-26 22:43:41 +0000

    Perl threaded http server based on 'HTTP::Daemon::Threaded' module.

---
+++ testing/threaded.pl
@@ -0,0 +1,57 @@
+#
+#       now fire up a server
+#
+package main;
+
+use HTTP::Daemon::Threaded;
+use HTTP::Daemon::Threaded::SessionCache;
+use MyContentParams;
+use MyEventLog;
+use MyWebLog;
+use TestCGI;
+
+use strict;
+use warnings;
+#
+# create a SessionCache object using default implementation
+#
+my $sessions = HTTP::Daemon::Threaded::SessionCache->new();
+#
+# create a ContentParams container
+#
+my $contparams = MyContentParams->new( "blah" );
+
+my $evtlog = MyEventLog->new();
+my $weblog = MyWebLog->new();
+
+my $httpd = HTTP::Daemon::Threaded->new(
+    Port                    => 8081,
+    MaxClients              => 20,
+    ContentParams   => $contparams,
+    SessionCache    => $sessions,
+    LogLevel                => 3,           # full info logging
+    EventLogger             => $evtlog,
+    WebLogger               => $weblog,
+    DocRoot                 => './',        # root directory for default file handler
+    Handlers                => [
+	'^\/posted$', 'TestCGI',                # uses CGI
+	'^\/postxml$', 'TestHTTPReq',   # uses HTTP::Request/Response directly
+	'^.*\/scripty\.js$', '*',       # default file handler
+	'^.*\/(\w|-)+\.html$', '*',         # default file handler
+	'^.*\/(\w|-)+\.css$', '*',         # default file handler
+	'^.*\/(\w|-)+\.fo$', '*',         # default file handler
+	'^.*\/(\w|-)+\.x[ms]l$', '*',         # default file handler
+	'^.*\/(\w|-|.)+?\.png$', '*',         # default file handler
+    ],
+    MediaTypes              => {
+	'text/xml'      => [ 'xml', 'dtd', 'fo' ],
+	'text/xsl'      => [ 'xsl' ],
+	'image/png'     => [ 'png' ],
+    }
+    ) || die "Unable to create web server, exiting.";
+
+while(1){};
+    #
+    # all done, shutdown
+    #
+    $httpd->shutdown();
